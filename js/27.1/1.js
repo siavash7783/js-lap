595793dca5cfbcf0784904d7e3888d13766a8dc8
siavash7783 <113995944+siavash7783@users.noreply.github.com>
2022-09-20 18:11:35 +0430

    Add files via upload

---
+++ js/27.1/1.js
@@ -0,0 +1,3 @@
+let f = prompt("enter a Fahrenheit ");
+c= (f-32)*5/9;
+console.log(c);
---
+++ js/27.2/2.js
@@ -0,0 +1,4 @@
+let weight = prompt("Enter a Weight : ");
+let height = prompt("Enter a height : ");
+bmi = (weight / Math.pow( (height/100), 2 ));
+console.log(bmi)
