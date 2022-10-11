77f100715d6ca5df6d03bafe2ca625ce8cdbe5da
siavash7783 <113995944+siavash7783@users.noreply.github.com>
2022-10-12 00:02:02 +0330

    Create 10.js

---
+++ js/31.1/10.js
@@ -0,0 +1,10 @@
+function randomString(len, charSet) {
+    charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var randomString = '';
+    for (var i = 0; i < len; i++) {
+        var randomPoz = Math.floor(Math.random() * charSet.length);
+        randomString += charSet.substring(randomPoz,randomPoz+1);
+    }
+    return randomString;
+}
+console.log(randomString(8))
